20293ad75d4fb1b1bb1f86b91c94bd3e55c95050
paige <88659700+paishee@users.noreply.github.com>
2024-01-23 10:39:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 class Payload {
-	constructor(base) {
+	constructor(base, ...baseArgs) {
 
 		// the actual data stuff
 		this.data = {};
-
-		
-		// promise proxy data
-		this.__promiseProxy = {
-
-			self: null,
-
-			// data stores
-			stores: {
-				data: [],
-				promise: []
-			}
-			
-		};
 
 
 		// the main return proxy
 		let stuff = new Proxy(
 
+
 			// the promise returned originally
 			Promise.resolve(new Proxy(this.data, {
-
+			
 				
 				// detects mostly when the promise is awaited
 				get: (...args) => {
-                    let stores = this.__promiseProxy.stores
-					stores.data = args;
-
-					const { data, promise } = stores;
 					const [ target, prop ] = args;
-
-					if (data.length > 0 && promise.length > 0) {
-                        Object.assign(target, base);
-                    }
-
 					return target[prop];
 				}
 			})), {
@@ -45,22 +23,16 @@ class Payload {
 
 			// detector for when something is called from the promise
 			get: (...args) => {
-                let stores = this.__promiseProxy.stores
-				stores.promise = args;
-
 				const [ target, prop ] = args;
 
-				// if the promise is awaited (then is called for await and .then )
                 if (prop == "then") {
-                    return target[prop].bind(base);
+                    return target[prop].bind(base(...baseArgs));
                 }
                 
                 return target[prop].bind(target);
 			}
 		});
 
-		
-		this.__promiseProxy.self = stuff;
 
 		return stuff;
 	}
@@ -69,11 +41,13 @@ class Payload {
 
 // inspect stuff
 try { 
+	
 	const util = require('util');
 	
 	Payload.prototype[util.inspect.custom] = function () {
 		return `Payload \x1b[33m\x1b[3m[pending]\x1b[0m`;
 	};
+	
 
 } catch(e) { }
 
